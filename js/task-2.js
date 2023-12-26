0dab940eb80c0b19245e53cfb5d8fcaaa15f2f75
AnnaVolkodav <annvolkodav@gmail.com>
2023-12-27 00:23:10 +0200

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,19 @@
 class Storage {
   #items;
   constructor(items) {
+    this.#items = items;
   }
   getItems() {
     return this.#items;
   }
-  addItem(item) {
-    this.#items.push(item);
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index > -1) {
+      this.#items.splice(index, 1);
+    }
   }
 }
 
@@ -14,5 +21,5 @@ const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-// storage.removeItem("Prolonger");
-// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
